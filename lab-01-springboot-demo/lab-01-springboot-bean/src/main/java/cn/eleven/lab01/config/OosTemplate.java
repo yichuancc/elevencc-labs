e948a5fd540c2ea7978fb107c77b86eb00915d3d
eleven <274224864@qq.com>
2023-09-30 21:11:41 +0800

    项目清理

--- lab-01-springboot-demo/lab-01-springboot-bean/src/main/java/cn/eleven/lab01/config/OosTemplate.java
+++
@@ -1,8 +0,0 @@
-package cn.eleven.lab01.config;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-
-@ConditionalOnProperty(prefix = FileServerProperties.PREFIX + "." + FileServerProperties.TYPE_OOS, name = "enabled", havingValue = "true",matchIfMissing = true)
-public class OosTemplate {
-
-}
