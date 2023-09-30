1e1099f279d7b0857a3f82263a236adc380b88df
eleven <274224864@qq.com>
2023-09-30 21:08:54 +0800

    项目清理

---
+++ lab-00-springboot/src/main/java/com/cc/lab00springboot/Lab00SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.cc.lab00springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab00SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab00SpringbootApplication.class, args);
+	}
+
+}
---
+++ lab-00-springboot/src/test/java/com/cc/lab00springboot/Lab00SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.lab00springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab00SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
