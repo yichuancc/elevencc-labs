76058ca6c66c10bc7e0814156b361f2d5b978050
eleven <274224864@qq.com>
2023-10-28 13:59:10 +0800

    数据库文档生成

---
+++ lab-02-database-word/src/main/java/com/cc/lab02databaseword/Lab02DatabaseWordApplication.java
@@ -0,0 +1,13 @@
+package com.cc.lab02databaseword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab02DatabaseWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab02DatabaseWordApplication.class, args);
+    }
+
+}
---
+++ lab-02-database-word/src/test/java/com/cc/lab02databaseword/Lab02DatabaseWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.lab02databaseword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab02DatabaseWordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
