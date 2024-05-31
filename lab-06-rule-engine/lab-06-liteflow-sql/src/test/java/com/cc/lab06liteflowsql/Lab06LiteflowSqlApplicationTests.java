1afdeb4b18778568590c1d19bf09e1a864ef2f34
eleven <274224864@qq.com>
2024-05-31 23:18:37 +0800

    规则引擎liteflow

---
+++ lab-06-rule-engine/lab-06-liteflow-sql/src/test/java/com/cc/lab06liteflowsql/Lab06LiteflowSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.lab06liteflowsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab06LiteflowSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
